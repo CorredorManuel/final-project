6308c05cbd467b8ee69c47439b144c471b6f893e
Miguelmalagon1020 <miguelmalagon1020@hotmail.com>
2022-05-20 23:34:56 -0500

    Update DATA.R

--- DATA.R
+++ DATA.R
@@ -7,12 +7,24 @@ tickers <- c("VSMPX","VFIAX","DODGX","SWPPX","RLBGX", "FSGEX","VGHAX","FPCIX",
              "AMECX","TRBCX","PIMIX","FBKWX")
 
 
-portfolioPrices <- NULL
+precios <- NULL
+retornos_activos <- NULL
+
 i <- 1
 for (activos in tickers){
-  portfolioPrices <- cbind(portfolioPrices, 
+  precios <- cbind(precios, 
                            getSymbols.yahoo(Symbols =activos,index.class  = 'Date',from ="2017-05-01",
-                                            to="2022-03-02",periodicity = "monthly",auto.assign = FALSE)[,6])
+                                           to="2022-03-02",periodicity = "monthly",auto.assign = FALSE)[,6])
+  
+  retornos_activos <- cbind(retornos_activos,monthlyReturn(portfolioPrices,leading = FALSE))
+  
   i <- i + 1
 }
+
+colnames(precios)<-tickers
+colnames(retornos_activos)<-tickers
+
+precios <- as.data.frame(precios)
+retornos_activos <- as.data.frame(retornos_activos)
+
 
